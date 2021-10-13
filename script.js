e14cb16b8c6a4d49803dab79dd877e381674ab01
Felipe Pilz <felipevilvertpilz@gmail.com>
2021-10-12 23:12:14 -0300

    Finished consulting the api and showing the pokemons

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+var quantity = document.getElementById("quantity");
+quantity.addEventListener('keyup', ()=>{
+	getPokemons(quantity.value);
+})
+
+function getPokemons(quantity){
+	fetch("https://pokeapi.co/api/v2/pokemon?limit="+quantity)
     .then((response) => response.json())
-    .then((allpokemon) => console.log(allpokemon));
+    .then((allpokemon) => {
+        var pokemons = [];
+        allpokemon.results.map((val) => {
+            fetch(val.url)
+                .then((response) => response.json())
+                .then((pokemonSingle) => {
+                    pokemons.push({
+                        name: val.name,
+                        img: pokemonSingle.sprites.front_default,
+                    });
+                    if (pokemons.length == quantity) {
+                        var pokemonsBoxes = document.querySelector(".pokemon-boxes");
+						pokemonsBoxes.innerHTML = "";
+                        pokemons.map((val) => {
+                            pokemonsBoxes.innerHTML +=
+                                `
+								<div class="pokemon-box col m-3 rounded shadow-sm" style="--bs-bg-opacity: 0.1; text-transform:capitalize;">
+                            		<h3 class="pt-4">` + val.name + `</h3>
+                            		<img style="width: 140px" src="`+val.img+`">
+                        		</div>
+							`;
+                        });
+                    }
+                });
+        });
+    });
+
+}
